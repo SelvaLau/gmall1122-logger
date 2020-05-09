48615b49b6e7ce6fefab6dec0e808df8deddbc75
Selva <Selva@sina.com>
2020-05-09 08:40:34 +0800

    Initial commit

---
+++ src/main/java/com/atguigu/gmall1122/logger/controller/LoggerController.java
@@ -0,0 +1,39 @@
+package com.atguigu.gmall1122.logger.controller;
+
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author Selva
+ * @date 2020/5/8 - 19:02
+ */
+@RestController
+@Slf4j
+public class LoggerController {
+
+    @Autowired
+    KafkaTemplate kafkaTemplate;
+
+    @RequestMapping("/applog")
+    public String applog(@RequestBody String logString) {
+
+        log.info(logString);
+        JSONObject jsonObject = JSON.parseObject(logString);
+        if (jsonObject.getString("start") != null && jsonObject.getString("start").length() > 0) {
+                kafkaTemplate.send("GMALL_START",logString);
+        }else {
+            kafkaTemplate.send("GMALL_EVENT",logString);
+
+        }
+
+        System.out.println(logString);
+        return "success";
+    }
+
+}
